ffe178439426cc36d35347fce91a46c1f851aece
ElvisAddo <addoelvis30@gmail.com>
2025-11-02 07:56:04 +0000

    modified the TodoItem component

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -2,19 +2,19 @@ import React from 'react';
 
 
 const TodoItem = React.memo(function TodoItem({ todo, onToggle, onRemove }) {
-return (
-<li className={`todo-item ${todo.completed ? 'completed' : ''}`}>
-<label className="todo-row">
-<input type="checkbox" checked={todo.completed} onChange={() => onToggle(todo.id)} />
-<span className="todo-text">{todo.text}</span>
-</label>
-<div className="todo-actions">
-<button className="btn small" onClick={() => onRemove(todo.id)} aria-label={`Remove ${todo.text}`}>
-Remove
-</button>
-</div>
-</li>
-);
+    return (
+        <li className={`todo-item ${todo.completed ? 'completed' : ''}`}>
+            <label className="todo-row">
+                <input type="checkbox" checked={todo.completed} onChange={() => onToggle(todo.id)} />
+                <span className="todo-text">{todo.text}</span>
+            </label>
+            <div className="todo-actions">
+                <button className="btn small" onClick={() => onRemove(todo.id)} aria-label={`Remove ${todo.text}`}>
+                    Remove
+                </button>
+            </div>
+        </li>
+    );
 });
 
 export default TodoItem;
