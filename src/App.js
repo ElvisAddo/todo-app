797e701749a59b1137b200cd9e8f79bf8da66677
ElvisAddo <addoelvis30@gmail.com>
2025-11-02 07:42:53 +0000

    added the app routes in the app entry point

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import TodosPage from './pages/TodosPage';
+import ContactPage from './pages/ContactPage';
+import NavBar from './components/NavBar';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default function App() {
+return (
+<div className="app-root">
+<NavBar />
+<main className="container">
+<Routes>
+<Route path="/" element={<Navigate to="/todos" replace />} />
+<Route path="/todos" element={<TodosPage />} />
+<Route path="/contact" element={<ContactPage />} />
+<Route path="*" element={<div>404</div>} />
+</Routes>
+</main>
+</div>
+);
 }
-
-export default App;
